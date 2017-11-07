1254e2cfa5b9bc8976e0d708dc741468c26f67d3
Min Kim <m-kim@kaist.ac.kr>
2017-11-08 01:47:05 +0900

    first try

--- run.c
+++ run.c
@@ -33,4 +33,29 @@ instruction* get_inst_info(uint32_t pc) {
 /***************************************************************/
 void process_instruction(){
 	/** Your implementation here */
+	instruction* instrp;
+	instrp = get_inst_info(CURRENT_STATE.PC);
+	instruction instr = *instrp;
+	printf(" op code is: %x\n", instr.opcode);
+	printf("pc is %x\n",CURRENT_STATE.PC);
+	IF_Stage();
+	sleep(1);
 }
+void IF_Stage(){
+	CURRENT_STATE.PIPE[1] = CURRENT_STATE.PIPE[0];
+	CURRENT_STATE.PIPE[0] = CURRENT_STATE.PC;
+	CURRENT_STATE.IF_ID_NPC = CURRENT_STATE.PC;
+	CURRENT_STATE.PC = CURRENT_STATE.PC +4;
+}
+void ID_Stage(){
+
+}
+void EX_Stage(){
+
+}
+void MEM_Stage(){
+
+}
+void WB_Stage(){
+
+}  
